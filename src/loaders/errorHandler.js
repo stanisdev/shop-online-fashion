bb9805549966ced78cf05ed982e445a6810b04e1
stanisdev <javascript.nodejs.developer@gmail.com>
2020-09-25 14:44:15 +0600

    Fix the main system error handler

--- src/loaders/errorHandler.js
+++ src/loaders/errorHandler.js
@@ -3,22 +3,33 @@
 const fp = require('fastify-plugin');
 const status = require('http-status');
 const createError = require('fastify-error');
-
+const isProduction = process.env.NODE_ENV === 'production';
 
 async function errorHandler(fastify) {
   fastify.decorate('createError', function(...args) {
     const CustomError = createError(...args);
-    return new CustomError();
+    const error = new CustomError();
+    error.validationContext = 'custom.definition';
+
+    return error;
   });
 
-  fastify.setErrorHandler(function (error, request, reply) {
-    const {
-      statusCode: code,
-      message
-    } = error instanceof Error && Number.isInteger(error.statusCode)
-      ? error
-      : { statusCode: status.INTERNAL_SERVER_ERROR, message: status[500] };
+  fastify.setErrorHandler((error, request, reply) => {
+    let code, message;
 
+    if (error instanceof Error) {
+      if (error.validationContext) {
+        code = Number.isInteger(error.statusCode) ? error.statusCode : status.BAD_REQUEST;
+        message = error.message;
+      } else {
+        code = status.INTERNAL_SERVER_ERROR;
+        message = isProduction ? status[500] : error.message;
+      }
+    } else {
+      code = status.INTERNAL_SERVER_ERROR;
+      message = isProduction ? status[500] : 'The thrown error is not instance of Error class';
+    }
+    fastify.log.error(error);
     reply
       .code(code)
       .send({
